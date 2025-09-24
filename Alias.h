a9cdb57ecfed6d36883d962d9acd45a58bcbef7d
ilovec0ncernn <kmoeahhbeat@gmail.com>
2025-09-24 12:24:41 +0300

    updated aliases

--- Alias.h
+++ Alias.h
@@ -2,7 +2,8 @@
 
 namespace nn {
 
-using Vector = Eigen::VectorXd; // NO MOVIES
-using Matrix = Eigen::MatrixXd; // IM LOCKED IN // IM IN THE TRENCHES
+using Vector = Eigen::VectorXd;  // NO MOVIES
+using Matrix = Eigen::MatrixXd;  // IM LOCKED IN
+using Index = Eigen::Index;      // IM IN THE TRENCHES
 
 }  // namespace nn
