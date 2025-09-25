873e2245b9c61e6b6c5e0d4508e06f615a68f085
ilovec0ncernn <kmoeahhbeat@gmail.com>
2025-09-25 16:01:31 +0300

    updated aliases

--- Alias.h
+++ Alias.h
@@ -1,9 +1,20 @@
+#pragma once
+
+#include <cstdint>
 #include <Eigen/Dense>
+#include <EigenRand/EigenRand>
 
 namespace nn {
 
-using Vector = Eigen::VectorXd;  // NO MOVIES
-using Matrix = Eigen::MatrixXd;  // IM LOCKED IN
-using Index = Eigen::Index;      // IM IN THE TRENCHES
+using Scalar = float;
+using Vector = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;               // NO MOVIES
+using Matrix = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;  // IM LOCKED IN
+using Index = Eigen::Index;                                            // IM IN THE TRENCHES
+
+struct RNG {
+    Eigen::Rand::P8_mt19937_64 gen;
+    explicit RNG(std::uint64_t seed = 42u) : gen(seed) {
+    }
+};
 
 }  // namespace nn
